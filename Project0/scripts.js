9e3ff160ef3fdbc1c6538388714739666cf60a9a
Norman Breuer <breuernorm@gmail.com>
2022-03-26 16:12:38 -0400

    added css blob animations, changed header style

--- Project0/scripts.js
+++ Project0/scripts.js
@@ -1,7 +1,19 @@
 
 /*
     Created By: Norman Breuer
-    Last Updated: 03/24/2022
+    Last Updated: 03/26/2022
     Description: Javascript file for project0 - "Random Movie Generator"
 */
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY'
+	}
+};
+
+fetch('https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
