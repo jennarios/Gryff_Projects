bf3d9c9e570e3ce12ca2e6d2922ed8097e17dbe1
Norman Breuer <breuernorm@gmail.com>
2022-03-27 00:18:34 -0400

    changed project idea and background, added input box and footer

--- Project0/scripts.js
+++ Project0/scripts.js
@@ -1,19 +1,27 @@
 
 /*
     Created By: Norman Breuer
-    Last Updated: 03/26/2022
-    Description: Javascript file for project0 - "Random Movie Generator"
+    Last Updated: 03/27/2022
+    Description: Javascript file for project0 - "Moviez - Movie Search Engine"
 */
 
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
-		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY'
+		'X-RapidAPI-Key': 'f960d4fb87msh6597a053ce51dcep1522f9jsn781acda7cdaa'
 	}
 };
 
-fetch('https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1', options)
+fetch('https://movie-database-alternative.p.rapidapi.com/?s=Alien&r=json&page=1', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+
+let $copyright = 
+    `<div id="copyright">
+        <p>Copyright © ${new Date().getFullYear()} Norman Breuer. Website by Norman Breuer.</p>
+    </div>`
+
+document.querySelector('#copyright').innerHTML = $copyright;
