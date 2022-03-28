67a275533f81dd1f0c1576a3b4e5fe734c867a71
Norman Breuer <breuernorm@gmail.com>
2022-03-27 23:21:09 -0400

    added more js and function comments

--- Project0/scripts.js
+++ Project0/scripts.js
@@ -2,22 +2,63 @@
 /*
     Created By: Norman Breuer
     Last Updated: 03/27/2022
-    Description: Javascript file for project0 - "Moviez - Movie Search Engine"
+    Description: Javascript file for project0 - "Musicaly - Music Artist Search"
 */
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
-		'X-RapidAPI-Key': 'f960d4fb87msh6597a053ce51dcep1522f9jsn781acda7cdaa'
-	}
-};
+// API FETCH FUNCTION TO GET ARTIST ID
 
-fetch('https://movie-database-alternative.p.rapidapi.com/?s=Alien&r=json&page=1', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+document.querySelector('#btn').addEventListener("click", getId);
 
+async function getId() {
+    let searchInput = document.querySelector('#search-box').value;
+    console.log(searchInput);
+
+    var options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+            'X-RapidAPI-Key': 'f960d4fb87msh6597a053ce51dcep1522f9jsn781acda7cdaa'
+        }
+    };
+
+    let res = await fetch(`https://spotify23.p.rapidapi.com/search/?q=${searchInput}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
+        .then(response => response.json())
+        .then(response => artistId(response))
+        .catch(err => console.error(err));
+
+        getOverview();
+    return res;
+}
+
+// FUNCTION TO GET ID
+
+    function artistId(res) {
+    let id = res.artists.items[0].data.uri;
+    let parsedId = id.replace("spotify:artist:","");
+    console.log(id);
+    console.log(id.replace("spotify:artist:",""))
+    return parsedId;
+}
+
+// FUNCTION TO GET ARTIST OVERVIEW
+
+async function getOverview() {
+    let options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+            'X-RapidAPI-Key': 'f960d4fb87msh6597a053ce51dcep1522f9jsn781acda7cdaa'
+        }
+    };
+
+    let res = await fetch(`https://spotify23.p.rapidapi.com/artist_overview/?id=${parsedId}`, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+    return res;
+}
+
+// FUNCTION TO ADD COPYRIGHT AT BOTTOM OF PAGE
 
 let $copyright = 
     `<div id="copyright">
@@ -25,3 +66,31 @@ let $copyright =
     </div>`
 
 document.querySelector('#copyright').innerHTML = $copyright;
+
+
+// let res = await fetch(`https://spotify23.p.rapidapi.com/search/?q=${searchInput}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
+// .then(response => response.json())
+// .then(response => artistName(response))
+// .then(response => artistImage(response))
+// .catch(err => console.error(err));
+// return res;
+
+// FUNCTION TO GET ARTIST NAME
+
+// function artistName(res) {
+//     let name = document.querySelector('#artist-name');
+//     name.innerHTML = res.artists.items[0].data.profile.name;
+//     console.log(res);
+//     return res;
+// }
+
+// FUNCTION TO GET ARTIST IMAGE
+
+// function artistImage(res) {
+//     let img = document.querySelector('#artist-img');
+//     img.setAttribute("src", "res.artists.items[0].data.visuals.avatarImage.sources[0]");
+//     console.log(res);
+//     return res;
+// }
+
+
