7cdc114a897bb43d3320014e4e3f24c60cc9a31f
Norman Breuer <breuernorm@gmail.com>
2022-03-29 00:14:35 -0400

    finished styling page, added more js functions

--- Project0/scripts.js
+++ Project0/scripts.js
@@ -1,96 +1,86 @@
-
 /*
     Created By: Norman Breuer
     Last Updated: 03/27/2022
     Description: Javascript file for project0 - "Musicaly - Music Artist Search"
 */
 
-// API FETCH FUNCTION TO GET ARTIST ID
+// GLOBAL VARIABLES
 
-document.querySelector('#btn').addEventListener("click", getId);
+document.querySelector("#btn").addEventListener("click", getId);
+
+const options = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+        "X-RapidAPI-Key": "f960d4fb87msh6597a053ce51dcep1522f9jsn781acda7cdaa",
+    },
+};
+
+// API FETCH FUNCTIONS
 
 async function getId() {
-    let searchInput = document.querySelector('#search-box').value;
+    let searchInput = document.querySelector("#search-box").value;
     console.log(searchInput);
 
-    var options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
-            'X-RapidAPI-Key': 'f960d4fb87msh6597a053ce51dcep1522f9jsn781acda7cdaa'
-        }
-    };
+    let firstJson = await fetch(
+        `https://spotify23.p.rapidapi.com/search/?q=${searchInput}&type=multi&offset=0&limit=10&numberOfTopResults=5`,
+        options
+    )
+        .then((response) => response.json())
+        .then((response) => getArtistId(response))
+        .then((response) => getOverview(response))
+        .catch((err) => console.error(err));
+    return firstJson;
+}
 
-    let res = await fetch(`https://spotify23.p.rapidapi.com/search/?q=${searchInput}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
-        .then(response => response.json())
-        .then(response => artistId(response))
-        .catch(err => console.error(err));
-
-        getOverview();
-    return res;
+async function getOverview(parsedId) {
+    let secondJson = await fetch(
+        `https://spotify23.p.rapidapi.com/artist_overview/?id=${parsedId}`,
+        options
+    );
+    let response = await secondJson.json();
+    setArtistName(response);
+    setArtistBio(response);
+    setArtistImage(response);
+    console.log(response, name);
+    // .catch(err => console.error(err));
+    return secondJson;
 }
 
 // FUNCTION TO GET ID
-
-    function artistId(res) {
-    let id = res.artists.items[0].data.uri;
-    let parsedId = id.replace("spotify:artist:","");
+function getArtistId(json) {
+    let id = json.artists.items[0].data.uri;
+    let parsedId = id.replace("spotify:artist:", "");
     console.log(id);
-    console.log(id.replace("spotify:artist:",""))
+    console.log(id.replace("spotify:artist:", ""));
     return parsedId;
 }
 
-// FUNCTION TO GET ARTIST OVERVIEW
+// FUNCTION TO GET ARTIST NAME
+function setArtistName(response) {
+    let name = document.querySelector("#artist-name");
+    name.innerHTML = response.data.artist.profile.name;
+    console.log(response);
+}
 
-async function getOverview() {
-    let options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
-            'X-RapidAPI-Key': 'f960d4fb87msh6597a053ce51dcep1522f9jsn781acda7cdaa'
-        }
-    };
+// FUNCTION TO GET ARTIST BIO
+function setArtistBio(res) {
+    let bio = document.querySelector("#bio");
+    bio.innerHTML = res.data.artist.profile.biography.text;
+    console.log(res);
+    return bio;
+}
 
-    let res = await fetch(`https://spotify23.p.rapidapi.com/artist_overview/?id=${parsedId}`, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-    return res;
+// FUNCTION TO GET ARTIST IMAGE
+function setArtistImage(json) {
+    let img = document.querySelector("#artist-img");
+    let url = json.data.artist.visuals.avatarImage.sources[0].url;
+    img.style.backgroundImage = `url('${url}')`;
 }
 
 // FUNCTION TO ADD COPYRIGHT AT BOTTOM OF PAGE
+const $copyright = `<div id="copyright">
+        <p>Copyright © ${new Date().getFullYear()} Norman Breuer</p>
+    </div>`;
 
-let $copyright = 
-    `<div id="copyright">
-        <p>Copyright © ${new Date().getFullYear()} Norman Breuer. Website by Norman Breuer.</p>
-    </div>`
-
-document.querySelector('#copyright').innerHTML = $copyright;
-
-
-// let res = await fetch(`https://spotify23.p.rapidapi.com/search/?q=${searchInput}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
-// .then(response => response.json())
-// .then(response => artistName(response))
-// .then(response => artistImage(response))
-// .catch(err => console.error(err));
-// return res;
-
-// FUNCTION TO GET ARTIST NAME
-
-// function artistName(res) {
-//     let name = document.querySelector('#artist-name');
-//     name.innerHTML = res.artists.items[0].data.profile.name;
-//     console.log(res);
-//     return res;
-// }
-
-// FUNCTION TO GET ARTIST IMAGE
-
-// function artistImage(res) {
-//     let img = document.querySelector('#artist-img');
-//     img.setAttribute("src", "res.artists.items[0].data.visuals.avatarImage.sources[0]");
-//     console.log(res);
-//     return res;
-// }
-
-
+document.querySelector("footer").innerHTML = $copyright;
